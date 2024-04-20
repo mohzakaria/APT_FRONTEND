3f8b8e01fbd7596c1a2a6e5a870c12971d0572ce
mohzakaria <95968167+mohzakaria@users.noreply.github.com>
2024-04-20 17:32:18 +0200

    weqw

--- src/components/NavbarElements.js
+++ src/components/NavbarElements.js
@@ -45,11 +45,6 @@ export const NavMenu = styled.div`
     display: flex;
     align-items: center;
     margin-right: -24px;
-    /* Second Nav */
-    /* margin-right: 24px; */
-    /* Third Nav */
-    /* width: 100vw;
-  white-space: nowrap; */
     @media screen and (max-width: 768px) {
         display: none;
     }
@@ -59,9 +54,6 @@ export const NavBtn = styled.nav`
     display: flex;
     align-items: center;
     margin-right: 20px;
-    /* Third Nav */
-    /* justify-content: flex-end;
-  width: 100vw; */
     @media screen and (max-width: 768px) {
         display: none;
     }
